c6c2ef3aab0aed99c8481832b4fa7c3ffbe28181
acydcross <acydcross@hotmail.com>
2020-09-29 19:25:52 -0600

    skeletal sign up form page done

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <p>Email</p>
+    <input type="text" name="" id="email-input" v-model="email" />
+    <p>Username</p>
+    <input type="text" name="" id="username-input" v-model="username" />
+    <p>Password</p>
+    <input type="password" name="" id="password-input" v-model="password" />
+    <p>Bio</p>
+    <textarea id="bio-input" v-model="bio"></textarea>
+    <p>Birthday</p>
+    <input type="text" name="" id="birthday-input" v-model="birthday" />
+    <h2 @click="signupUser">Sign Up</h2>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "sigup-form",
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+      bio: "",
+      birthday: ""
+    };
+  },
+  methods: {
+    signupUser: function() {
+      axios
+        .request({
+          method: "POST",
+          url: "https://tweeterest.ml/api/users",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": "fOytoHincX2T35xWW2Dmx6U6D21Q2Z2DedeWJPo9F3hLC"
+          },
+          data: {
+            email: this.email,
+            username: this.username,
+            password: this.password,
+            bio: this.bio,
+            birthdate: this.birthday
+          }
+        })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Sign Up Page</h1>
+    <signup-form></signup-form>
+  </div>
+</template>
+
+<script>
+import SignupForm from "../components/SignupForm.vue";
+
+export default {
+  name: "signup-page",
+  components: {
+    SignupForm
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
